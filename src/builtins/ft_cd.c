192feb869e69f65812950e4c0ef2a79b81ca0cc9
krokra <95774040+krokra@users.noreply.github.com>
2025-04-14 19:07:23 +0200

    ft_cd changes
    handled tilde and dash

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -12,16 +12,30 @@
 
 #include "../../includes/minishell.h"
 
-void	ft_cd(char *str, char **env)
+static int	special_cases(char *str, char **env)
 {
-	if (str == NULL)
+	if (str == NULL || ft_strncmp(str, "~", 1) == 0)
 		if (chdir(getenv("HOME")) != -1)
 		{
 			oldpwd_update(env, getenv("PWD"));
 			pwd_update(env, getenv("HOME"));
-			return ;
+			return (1);
 		}
-	if (chdir(str) != -1 && str != NULL)
+	if (ft_strncmp(str, "-", 1) == 0)
+		if (chdir(getenv("OLDPWD")) != -1)
+		{
+			oldpwd_update(env, getenv("PWD"));
+			pwd_update(env, getenv("OLDPWD"));
+			return (1);
+		}
+	return (0);
+}
+
+void	ft_cd(char *str, char **env)
+{
+	if ((str == NULL || ft_strncmp(str, "~", 1) == 0 || ft_strncmp(str, "-", 1) == 0) && special_cases(str, env) != 0)
+		return ;
+	else if (chdir(str) != -1 && str != NULL)
 	{
 		oldpwd_update(env, getenv("PWD"));
 		pwd_update(env, str);
